fc83226ed5e754192d246a0ff5015fdeb2ab304f
rrt <rrt@odoo.com>
2020-09-21 18:26:58 +0530

    Working on proposal template

---
+++ praposal_app/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Praposal Application",
+    'sequence':'1',
+    'summary': "Proposal Application",
+    'description': "This app for the sent the proposal to customer for the products",
+    'author': "Odoo India ,Roopam Rathod(rrt)",
+    'website': "http://www.odoo.com",
+    'category': 'Uncategorized',
+    'version': '12.0.0.1',
+    'depends': ['base','contacts','hr','sale','payment','portal','sales_team'],
+    'data': [
+        'data/sequence.xml',
+        'views/portal_template.xml',
+        'report/proposal_report.xml',
+        'security/ir.model.access.csv',
+        'views/views.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
---
+++ praposal_app/controllers/portalproposal.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+from datetime import date
+from odoo import fields, http, _
+from odoo.exceptions import AccessError, MissingError
+from odoo.http import request
+from odoo.addons.payment.controllers.portal import PaymentProcessing
+from odoo.addons.portal.controllers.mail import _message_post_helper
+from odoo.addons.portal.controllers.portal import CustomerPortal, pager as portal_pager, get_records_pager
+from odoo.osv import expression
+
+class CustomerProposal(CustomerPortal):
+    
+    @http.route(['/my/proposal/<int:proposal_id>'], type='http', auth="public", website=True)
+    def portal_order_page(self,proposal_id, report_type=None, access_token=None, message=False, download=False, **kw):
+        try:
+            order_sudo = self._document_check_access('proposal.order', proposal_id, access_token=access_token)
+        except (AccessError, MissingError):
+            return request.redirect('/my')
+
+        if report_type in ('html', 'pdf', 'text'):
+            return self._show_report(model=order_sudo, report_type=report_type, report_ref='sale.action_report_saleorder', download=download)
+
+        if order_sudo:
+            now = fields.Date.today().isoformat()
+            session_obj_date = request.session.get('view_quote_%s' % order_sudo.id)
+            if isinstance(session_obj_date, date):
+                session_obj_date = session_obj_date.isoformat()
+            if session_obj_date != now and request.env.user.share and access_token:
+                request.session['view_quote_%s' % order_sudo.id] = now
+                body = _('Proposal viewed by customer')
+                _message_post_helper(res_model='proposal.order', res_id=order_sudo.id, message=body, token=order_sudo.access_token, message_type='notification', subtype="mail.mt_note", partner_ids=order_sudo.user_id.sudo().partner_id.ids)
+
+        values = {
+            'proposal_order': order_sudo,
+            'message': message,
+            'token': access_token,
+            'return_url': '/shop/payment/validate',
+            'bootstrap_formatting': True,
+            'partner_id': order_sudo.partner_id.id,
+            'report_type': 'html',
+        }
+        if order_sudo.company_id:
+            values['res_company'] = order_sudo.company_id
+
+        return request.render('praposal_app.proposal_order_portal_template', values)                            
+        
