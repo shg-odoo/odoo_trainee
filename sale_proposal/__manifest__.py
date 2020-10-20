508fed5ddbf20a161d4a6c26bbc86cc604f632e4
ija-odoo <ija@odoo.com>
2020-10-20 19:05:36 +0530

    working with email

--- sale_proposal/__manifest__.py
+++ sale_proposal/__manifest__.py
@@ -2,10 +2,13 @@
     'name': 'Sale Proposal',
     'version': '1.0',
     'description': """Sale Proposal""",
-    'depends': ['sale'],
+    'depends': ['sale','mail','website','portal'],
     'data': [
         'security/ir.model.access.csv',
-        'views/sale_proposal_view.xml'
+        'views/sale_proposal_view.xml',
+        'data/sequence.xml',
+        'data/mail_template.xml',
+
     ],
     'installable': True,
 }
--- sale_proposal/models/sale_proposal.py
+++ sale_proposal/models/sale_proposal.py
@@ -3,11 +3,12 @@ from odoo import models, fields, api, _
 
 class SaleProposal(models.Model):
     _name = "sale.proposal"
+    _inherit = ['portal.mixin', 'mail.thread', 'mail.activity.mixin']
     _rec_name = "partner_id"
 
-    user_id = fields.Many2one('res.users', string="Salesman",default=lambda self: self.env.user)
+    user_id = fields.Many2one('res.users', string="Salesman", default=lambda self: self.env.user)
     partner_id = fields.Many2one('res.partner', string="Customer")
-    proposal_line_ids=fields.One2many('proposal.lines','proposal')
+    proposal_line_ids = fields.One2many('proposal.lines', 'proposal')
     state = fields.Selection([
         ('draft', 'Draft'),
         ('sent', 'Sent'),
@@ -18,22 +19,103 @@ class SaleProposal(models.Model):
                                   default=lambda self: self.env.user.company_id.currency_id.id, required=True)
     name_seq = fields.Char(string="Proposal Sequence", copy=False, required=True, index=True,
                            default=lambda self: _('New'))
+    date_proposal = fields.Datetime(string='Proposal Date', required=True, readonly=True, index=True, states={'draft': [('readonly', False)], 'sent': [('readonly', False)]}, copy=False, default=fields.Datetime.now)
+    # amount_untaxed = fields.Monetary(string='Untaxed Amount', store=True, readonly=True, compute='_amount_all', tracking=5)
+    # amount_tax = fields.Monetary(string='Taxes', store=True, readonly=True, compute='_amount_all')
+    proposed_total = fields.Monetary(string='Proposed Total', store=True, readonly=True, compute='_amount_total')
+    accepted_total = fields.Monetary(string='Accepted Total', store=True, readonly=True, compute='_amount_total')
+    total=fields.Monetary(string='Total', store=True, readonly=True, compute='_amount_total')
+
+    def action_send_mail(self):
+        print('ooooooooooooooo')
+        template_id = self.env.ref('sale_proposal.mail_proposal_template').id
+        template = self.env['mail.template'].browse(template_id)
+        template.send_mail(self.id, force_send=True)
+        print(template)
+        return self.write({'state': 'sent'})
+        # self.ensure_one()
+        # self.write({'state': 'sent'})
+        #
+        # ir_model_data = self.env['ir.model.data']
+        # try:
+        #
+        #     template_id = ir_model_data.get_object_reference('sale_proposal', 'mail_proposal_template')[1]
+        # except ValueError:
+        #
+        #     template_id = False
+        #
+        # ctx = {
+        #     'default_model': 'sale.proposal',
+        #     'default_res_id': self.ids[0],
+        #     'default_use_template': bool(template_id),
+        #     'default_template_id': template_id,
+        #     'default_composition_mode': 'comment',
+        # }
+        # print(ctx)
+
+    @api.model
+    def create(self, vals):
+        if vals.get('name_seq', _('New')) == _('New'):
+            vals['name_seq'] = self.env['ir.sequence'].next_by_code('sale.proposal') or _('New')
+        result = super(SaleProposal, self).create(vals)
+        return result
+
+
+
+
+    @api.depends('proposal_line_ids.amt_total_proposed','proposal_line_ids.amt_total_accepted')
+    def _amount_total(self):
+                for order in self:
+                    proposed_total = 0.0
+                    accepted_total = 0.0
+                    for line in order.proposal_line_ids:
+                        proposed_total += line.amt_total_proposed
+                        accepted_total += line.amt_total_accepted
+                    order.update({
+                        'proposed_total': proposed_total,
+                        'accepted_total': accepted_total,
+                        'total': proposed_total + accepted_total
+                    })
+
+
+    def action_confirm(self):
+        self.write({'state': 'confirm'})
+
+    def action_send(self):
+            self.write({'state': 'sent'})
+
+    def action_cancell(self):
+        self.write({'state': 'cancel'})
+
 
 class ProposalLines(models.Model):
     _name = "proposal.lines"
 
-    Product_id = fields.Many2one('product.product', string='Product')
+    product_id = fields.Many2one('product.product', string='Product')
     label = fields.Text(string='Description', required=True)
-    qty_proposed=fields.Integer(string="Quantity Proposed")
-    qty_accepted=fields.Integer(string="Quantity Accepted")
-    price_proposed=fields.Float(string="Price Proposed")
-    price_accepted=fields.Float(string="Price Accepted")
-    amt_total_proposed=fields.Float(string="Amount total proposed")
-    amt_total_accepted=fields.Float(string="Amount total accepted")
-    proposal=fields.Many2one('sale.proposal')
+    qty_proposed = fields.Integer(string="Quantity Proposed",default="1")
+    qty_accepted = fields.Integer(string="Quantity Accepted")
+    price_proposed = fields.Float(string="Price Proposed")
+    price_accepted = fields.Float(string="Price Accepted")
+    amt_total_proposed = fields.Float(string="Amount total proposed")
+    amt_total_accepted = fields.Float(string="Amount total accepted")
+    proposal = fields.Many2one('sale.proposal')
     company_id = fields.Many2one('res.company', 'Company', default=lambda self: self.env.user.company_id.id, index=1)
     currency_id = fields.Many2one('res.currency', 'Currency',
                                   default=lambda self: self.env.user.company_id.currency_id.id, required=True)
+
+
+    @api.onchange('product_id')
+    def _onchange_product_id(self):
+       self.price_proposed=self.product_id.lst_price
+       self.label=self.product_id.name
+
+    @api.onchange('qty_proposed','price_proposed')
+    def _onchange_proposal(self):
+        self.amt_total_proposed= self.qty_proposed * self.price_proposed
+    @api.onchange('qty_accepted','price_accepted')
+    def _onchange_proposal_accepted(self):
+        self.amt_total_accepted= self.qty_accepted * self.price_accepted
 
 
 
