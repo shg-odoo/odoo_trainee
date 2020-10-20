95914f687ab42e70efcc1d100c32005a98f1bb44
Suhani Modi <sod@odoo.com>
2020-10-20 18:40:18 +0530

    [ADD] my-modules: create new module

---
+++ my-modules/academy/models/models.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+class Teachers(models.Model):
+    _name = 'academy.teachers'
+
+    name = fields.Char()
+    biography = fields.Html()
+    
+    
+    course_ids = fields.One2many('academy.courses', 'teacher_id', string="Courses")
+
+class Course(models.Model):
+    _name = 'academy.courses'
+
+    name = fields.Char()
+    teacher_id = fields.Many2one('academy.teachers', string="Teacher")
+
+#     _description = 'academy.academy'
+
+#     name = fields.Char()
+#     value = fields.Integer()
+#     value2 = fields.Float(compute="_value_pc", store=True)
+#     description = fields.Text()
+#
+#     @api.depends('value')
+#     def _value_pc(self):
+#         for record in self:
+#             record.value2 = float(record.value) / 100
