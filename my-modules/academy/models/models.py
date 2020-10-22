3eb120c982889410df189445bd2dd7e60f711526
Suhani Modi <sod@odoo.com>
2020-10-22 18:18:23 +0530

    [ADD] my-modules: create new module

--- my-modules/academy/models/models.py
+++ my-modules/academy/models/models.py
@@ -8,24 +8,8 @@ class Teachers(models.Model):
     name = fields.Char()
     biography = fields.Html()
     
-    
-    course_ids = fields.One2many('product.template', 'teacher_id', string="Courses")
+    course_ids = fields.One2many('academy.courses', 'teacher_id', string="Courses")
 
 class Course(models.Model):
-#    _name = 'academy.courses'
-#    _inherit = 'mail.thread'
     _inherit = 'product.template'
-#    name = fields.Char()
     teacher_id = fields.Many2one('academy.teachers', string="Teacher")
-
-#     _description = 'academy.academy'
-
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
