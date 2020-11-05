e50f99d1c671d124d946990d57155dfc93a37f60
Anjali <aaj@odoo.com>
2020-11-05 18:04:35 +0530

    [ADD] test_module

---
+++ test-module/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'Test Module',
+    'category': 'Sales',
+    'summary': 'Manage accounting with fleets',
+    'description': "",
+    'version': '1.0',
+    'depends': ['sale'],
+    'data': [
+
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+
+}
