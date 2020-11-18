71d3e53b63c54f31c2f46e97b476d06818d34cc7
Anjali <aaj@odoo.com>
2020-11-18 22:58:51 +0530

    [ADD]

--- product_proposal/static/src/js/proposal.js
+++ product_proposal/static/src/js/proposal.js
@@ -1,6 +1,9 @@
 odoo.define('product_proposal.portal', function (require) {
     'use strict';
 
+    var publicWidget = require('web.public.widget');
+    var ajax = require('web.ajax');
+
 window.addEventListener('load',function(e){
     console.log('document.onload');
 
@@ -23,16 +26,8 @@ window.addEventListener('load',function(e){
            e.target.previousElementSibling.value ++
                                                         });
                                          }
-
-    var sumTotal =0.0;
-//      for (price of accept_price){
-//      price.addEventListener("oninput", function(e){
-//        sumTotal = sumTotal+price.value
-//      });
-//      }
-//
-
-//
+//on change for both qty change and accepted total
+//    var sumTotal =0.0;
 //const accepted_qty = document.querySelector('.accepted_quantity')
 //
 //const element2 = document.querySelector('.accept_price')
@@ -44,13 +39,26 @@ window.addEventListener('load',function(e){
 //  //handle  change
 //  console.log("event...........")
 //  console.log("accepted_qty....",accepted_qty)
-//
-
-
-
-
 
 
     });
+
+  publicWidget.registry.ProposalWidget = publicWidget.Widget.extend({
+        selector: '#accept',
+        events: {
+            'click .accept_button': 'clicking_accept_btn',
+        },
+        clicking_accept_btn: function () {
+            ajax.jsonRpc('/my/proposals/<int:order_id>/accept', 'call', {
+                'data': dict, 'access_token': access_token
+            },
+            location.reload())
+
+        },
+    })
 });
+
+
+
+//});
 
