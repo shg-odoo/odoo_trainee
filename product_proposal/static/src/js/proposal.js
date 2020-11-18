06bc1c0e9294b11490ee81c54103c4e8da28b280
Anjali <aaj@odoo.com>
2020-11-18 10:47:55 +0530

    [ADD]

--- product_proposal/static/src/js/proposal.js
+++ product_proposal/static/src/js/proposal.js
@@ -6,45 +6,51 @@ window.addEventListener('load',function(e){
 
    var elementsMin = document.getElementsByClassName("minus");
    var elementsPlu = document.getElementsByClassName("plus");
-    var accept_price = document.getElementsByClassName("accept_price");
-   console.log("price.....",accept_price)
-   console.log('elementsMin................',elementsMin)
-   var element,closestMin,closestPlu
+   var accept_price = document.getElementsByClassName("accept_price");
+
+   var element;
 
        for (element of elementsMin) {
         element.addEventListener("click", function(e){
-        console.log('inner funcn', e.target);
-//        console.log('inner elemnt', e.nextElementSibling);
-        closestMin = element.nextElementSibling("accepted_quantity");
-        console.log("closest..",closest)
-        if (closestMin.value > 0 ){
-                        closestMin.value --
+        if (e.target.nextElementSibling.value > 0 ){
+                        e.target.nextElementSibling.value --
                                    }
                                                     });
                                      }
 
-
-
-//        input_text = document.querySelectorAll(".number > .accepted_quantity");
-//        console.log("input_text............",input_text)
-//        var list = document.getElementById("minus-btn").nextSibling.innerHTML;
-//        console.log("lilst.............",list)
-
-
-
           for (element of elementsPlu) {
            element.addEventListener("click", function(e){
-           var closestPlu = element.ElementSibling("accepted_quantity");
-           closestPlu.value ++
+           e.target.previousElementSibling.value ++
                                                         });
                                          }
 
-      var sumTotal =0.0;
-      for (price of accept_price){
-      price.addEventListener("oninput", function(e){
-        sumTotal = sumTotal+price.value
-      });
-      }
+    var sumTotal =0.0;
+//      for (price of accept_price){
+//      price.addEventListener("oninput", function(e){
+//        sumTotal = sumTotal+price.value
+//      });
+//      }
+//
+
+//
+//const accepted_qty = document.querySelector('.accepted_quantity')
+//
+//const element2 = document.querySelector('.accept_price')
+//console.log('!!!!', element2)
+//document.addEventListener('change', event => {
+//  if (event.target !== accepted_qty && event.target !== element2) {
+//    return
+//  }
+//  //handle  change
+//  console.log("event...........")
+//  console.log("accepted_qty....",accepted_qty)
+//
+
+
+
+
+
 
     });
 });
+
