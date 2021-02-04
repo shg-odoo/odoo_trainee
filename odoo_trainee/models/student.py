77526c83dcf0ca9c939d3d595328bd4b4e08bddb
kpt-odoo <kpt@odoo.com>
2021-02-04 16:35:42 +0530

    Graph and Calender view

--- odoo_trainee/models/student.py
+++ odoo_trainee/models/student.py
@@ -10,4 +10,12 @@ class student(models.Model):
     contactNo = fields.Char(string="Contact No")
     email = fields.Char(string="Email Id")
     branch = fields.Char(string="Branch")
+    per = fields.Integer(string="Percentage")
+    bdate = fields.Date(string='Date of birth')
+    gender = fields.Selection([ ('male', 'Male'),('female', 'Female'),],'Gender', default='male')
+    image = fields.Binary(string='Image')
+   
+
+
     
+
