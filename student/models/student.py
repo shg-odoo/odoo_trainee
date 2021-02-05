f39c59aa56b88c129f4262df7b815c23b13a7142
kcv-odoo <kcv@odoo.com>
2021-02-05 18:33:00 +0530

    add form and kanban views and menus in student module and calculate avg use .py file

--- student/models/student.py
+++ student/models/student.py
@@ -8,15 +8,21 @@ class student(models.Model):
     roll_no = fields.Integer(string='Roll No')
     birthdate = fields.Date(string='birth date')
     image = fields.Binary()
-    gender = fields.Selection([('male','Male') ,('female','Female')],string='Gender', default='male')
+    gender = fields.Selection([('male','Male') ,('female','Female')],string='Gender', default="male")
     age = fields.Char(string='age')
     html = fields.Html()
+    branch = fields.Char(string="Branch")
     physics = fields.Integer(string='physics')
     maths = fields.Integer(string='maths')
     chemistry = fields.Integer(string='chemistry')
-    Average = fields.Float(string='Average')
+    Average = fields.Float(compute="_Avg",string='Average',store=True)
     Address = fields.Char(string='Address')
     current_date = fields.Date('Current Date',default = lambda cdate:fields.Date.today())
     contect_no = fields.Char(string='Contect No')
+
+    @api.constrains('physics','maths','chemistry')
+    def _Avg(self):
+        avg = (self.physics+self.chemistry+self.maths)/3
+        self.Average = avg
     
     
