63607bd86a4eb1c08459b03b971002d5f6fcaae3
vpt-odoo <vpt@odoo.com>
2021-02-08 18:30:13 +0530

    [IMP] Implemented Form View With New Fields (Image, Radio Button, Html)

--- Student_Trainee/models/models.py
+++ Student_Trainee/models/models.py
@@ -1,11 +1,16 @@
 from odoo import models,fields
 
 class Student(models.Model):
-    _name='student'
+    _name='school.student'
     _description='Student Details'
     
     name=fields.Char(string='Name')
     age=fields.Integer(string='Age')
     height=fields.Float(string='Height')
     birthDate=fields.Date(string='Birth Date')
-    
+    gender = fields.Selection([
+        ('male', 'Male'),
+        ('female', 'Female'),
+        ],string='Gender', default='male')
+    image=fields.Binary(string="Image")
+    html=fields.Html('Description')
