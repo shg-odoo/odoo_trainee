5e92f5f3ec209f0c438e66dd385cfa85c15235fa
vpt-odoo <vpt@odoo.com>
2021-02-10 18:53:14 +0530

    [IMP] age validation, calculate total and sum by onchange,created kanban view

--- Student_Trainee/models/models.py
+++ Student_Trainee/models/models.py
@@ -1,6 +1,7 @@
 from odoo import models,fields,api
 from datetime import date, datetime
 from dateutil.relativedelta import relativedelta
+from odoo.exceptions import ValidationError
 
 class Student(models.Model):
     _name='school.student'
@@ -24,14 +25,38 @@ class Student(models.Model):
     bloodGroup=fields.Selection([("o+","O+"),("o-","O-"),("b+","B+"),("b-","B-"),("a+","A+"),("a-","A-"),("ab+","AB+"),("ab-","AB-")],string="Blood Group")
     height=fields.Float(string="Height")
     weight=fields.Integer(string="Weight")
+    bmi=fields.Float(string="BMI")
     disabled=fields.Boolean(string="Physically Disabled?",default=False)
     
     intro=fields.Html('Introduction')
     
     date_today=fields.Date(default=lambda today:fields.date.today())
     
+    maths=fields.Integer(string="Maths")
+    chemistry=fields.Integer(string="Chemistry")
+    physics=fields.Integer(string="Physics")
+    total=fields.Integer(string="Total")
+    average=fields.Float(string="Average")
+    
     @api.depends("birthDate")
     def _get_age(self):
         for i in self:
             if i.birthDate:
                 i.age = relativedelta(date.today(),i.birthDate).years
+    
+    @api.constrains('age')
+    def _ageValidator(self):
+        if self.age<18:
+            raise ValidationError("Age Must Be 18 Or 18+")
+    
+    @api.onchange('weight','height')
+    def _calculateBMI(self):
+        for i in self:
+            i.bmi= (i.weight)/((i.height*0.3048)**2)
+    
+    @api.onchange('maths','chemistry','physics')
+    def _calculateResult(self):
+        for i in self:
+            i.total=i.maths+i.chemistry+i.physics
+            i.average=(i.total)/3
+        
