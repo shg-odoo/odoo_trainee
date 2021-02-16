f5ba9cfaf5f7a1cd807339fa7e3d90f757851f05
mte-odoo <mte@odoo.com>
2021-02-16 18:40:43 +0530

    Kanban view, Search view

--- Student_Management/models/student.py
+++ Student_Management/models/student.py
@@ -1,5 +1,5 @@
 from odoo import models, fields, api, exceptions
-from datetime import timedelta
+from datetime import timedelta, datetime, date
 
 class student(models.Model):
     _name = 'student'
@@ -7,11 +7,41 @@ class student(models.Model):
 
     name = fields.Char(string="Name")
     enrollmentNo = fields.Integer(string="Enrollment No")
-    contactNo = fields.Char(string="Contact No")
+    contactNo = fields.Char(string="Contact No", size=10) 
+    age = fields.Integer(string="Age")
     email = fields.Char(string="Email Id")
     branch = fields.Char(string="Branch")
     image = fields.Binary(string = "image")
     birthdate = fields.Date(string = "Date of Birth")
-    date_from = fields.Datetime(string="From")
-    date_to = fields.Datetime(string="To")
+    dateTo = fields.Date(string='Date', default=datetime.today())
+    gender = fields.Selection([
+            ('male', 'Male'),
+            ('female', 'Female'),
+        ], string='Gender', default='male')
+    city = fields.Char(string = "City")
+    bloodGroup = fields.Selection([
+            ('A+', 'A+'),
+            ('A-', 'A-'),
+            ('B+', 'B+'),
+            ('B-', 'B-'),
+            ('AB+', 'AB+'),
+            ('AB-', 'AB-'),
+            ('O+', 'O+'),
+            ('O-', 'O-'),
+        ], string='Blood Group', default='B+')
+
+    qualification = fields.Selection([
+            ('10th', '10th'),
+            ('12th', '12th'),
+            ('graduate', 'Graduate'),
+            ('post-graduate', 'Post-Graduate'),
+            ('phd', 'Phd'),
+        ], string='Qualification', default='10th')
+
+    address = fields.Text(string="Address")
+    pincode = fields.Char(string="Pincode")
+    country = fields.Char(string="Country")
+    weight = fields.Float(string="Weight")
+    height = fields.Float(string="Height")
+    disabled = fields.Boolean(string="Disabled", default=False)
     
