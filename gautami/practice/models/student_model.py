c77f909c618dd03e3bc09a155e6298edad16490f
Gautami Gamare <gag@odoo.com>
2021-05-20 18:44:34 +0530

    updated orm methods

--- gautami/practice/models/student_model.py
+++ gautami/practice/models/student_model.py
@@ -28,48 +28,60 @@ class StudentDetails(models.Model):
     def _compute_pname(self):
         self.student_percentage = ((self.fy_marks + self.sy_marks + self.ty_marks)/300)*100
 
-    # @api.model
-    # def _name_search(self,name='',args=None,operator='ilike',limit=100):
-    #     if args is None:
-    #         args = []
-    #     domain = args + [('student_name',operator,name)]
-    #     return super(StudentDetails,self).search(domain,limit=limit)
+    def name_get(self):
+        res = []
+        for analytic in self:
+            name = analytic.student_name
+            res.append((analytic.id, name))
+        return res 
 
     def action_confirm(self):
         for x in self:
             students = self.env['student.details'].search([])
             print("students---------->",students)
+            print("students---------->",students.ids)
+            print("students---------->",students.mapped(lambda r:r.student_name))
+            print("students---------->",students.sorted(lambda r:r.student_age).mapped(lambda r:r.student_name))
+            print("students---------->",students.sorted(lambda r:r.student_age,reverse=True).mapped(lambda r:r.student_name))
+            print("students---------->",students.filtered(lambda r:r.student_age>20 and r.student_age<30).mapped(lambda r:r.student_name))
+            print("students---------->",students.default_get(['gender']))
+            print("students---------->",students.read(['student_name','student_age']))
+            print("students---------->",students.name_get())
+            # print("students---------->",students.get_metadata())
+            print("students---------->",students[3].ensure_one())
             
-            female_students = self.env['student.details'].search([('gender','=','female')])
-            print("female students---------->",female_students)
             
-            male_students = self.env['student.details'].search([('gender','=','male')])
-            print("male students---------->",male_students)
-            
-            female_students_and_age = self.env['student.details'].search([('gender','=','female'),('student_age','>=','20')])
-            print("female students and age---------->",female_students_and_age)
-            
-            female_students_or_age = self.env['student.details'].search(['|',('gender','=','female'),('student_age','>=','20')])
-            print("female students or age---------->",female_students_or_age)
-            
-            cout_stud = self.env['student.details'].search_count([])
-            print("students count---------->",cout_stud)
 
-            cout_female_stud = self.env['student.details'].search_count([('gender','=','female')])
-            print("students female count---------->",cout_female_stud)
+            # female_students = self.env['student.details'].search([('gender','=','female')])
+            # print("female students---------->",female_students)
+            
+            # male_students = self.env['student.details'].search([('gender','=','male')])
+            # print("male students---------->",male_students)
+            
+            # female_students_and_age = self.env['student.details'].search([('gender','=','female'),('student_age','>=','20')])
+            # print("female students and age---------->",female_students_and_age)
+            
+            # female_students_or_age = self.env['student.details'].search(['|',('gender','=','female'),('student_age','>=','20')])
+            # print("female students or age---------->",female_students_or_age)
+            
+            # cout_stud = self.env['student.details'].search_count([])
+            # print("students count---------->",cout_stud)
 
-            studs_browse = self.env['student.details'].browse([3,4])
-            print(studs_browse)
+            # cout_female_stud = self.env['student.details'].search_count([('gender','=','female')])
+            # print("students female count---------->",cout_female_stud)
 
-            stud_browse = self.env['student.details'].browse(4)
-            if stud_browse.exists():
-                print(stud_browse)
-                for x in stud_browse:
-                    print(x.student_name)
-                    print("display name-------->",x.display_name)
+            # studs_browse = self.env['student.details'].browse([3,4])
+            # print(studs_browse)
+
+            # stud_browse = self.env['student.details'].browse(4)
+            # if stud_browse.exists():
+            #     print(stud_browse)
+            #     for x in stud_browse:
+            #         print(x.student_name)
+            #         print("display name-------->",x.display_name)
                     
-            else:
-                print("Record Doesn't exists")
+            # else:
+            #     print("Record Doesn't exists")
 
             #CREATE
 
@@ -102,8 +114,7 @@ class StudentDetails(models.Model):
             # stud_browse = self.env['student.details'].browse(11)
             # stud_browse.unlink()
 
-
-
+            
 class CollegeDetails(models.Model):
     _name = 'college.details'
     _description = 'College information is stored here'
@@ -115,22 +126,31 @@ class CollegeDetails(models.Model):
 
     def name_get(self):
         res = []
-        for rec in self:
-            res.append((rec.id,'%s - %s' % (rec.college_name,rec.college_city)))
+        for analytic in self:
+            name = analytic.college_name
+            city = analytic.college_city
+            res.append((analytic.id, '%s (%s)' % (name,city)))
         return res
 
     @api.model
-    def _name_search(self,name='',args=None,operator='ilike',limit=100):
-        if args is None:
-            args = []
-        domain = args + [('student_name',operator,name)]
-        return super(StudentDetails,self).search(domain,limit=limit).name_get()
+    def name_create(self,college_name):
+        rtn = self.create({'college_name':college_name})
+        return rtn.name_get()[0]
+
+    @api.model
+    def name_search(self,name,args=None,operator='ilike',limit=100):
+        args = args or []
+        if name:
+            records = self.search([('college_name',operator,name)])
+            return records.name_get()
+        return self.search([('college_name',operator,name)],limit=limit).name_get()
 
 class StudentHobbies(models.Model):
     _name = 'student.hobbies'
     _description = 'Students Hobbies list'
 
     name = fields.Char(string="Hobbies")
+
 
 class StudentEmail(models.Model):
     _inherit = 'student.details'
