8f4290bea19667f67e2a43591d31ae405c1394ee
visi-odoo <visi@odoo.com>
2021-07-21 19:04:43 +0530

    forms , images , selection today's task done

--- week_2/trainee_demo/models/st_detail.py
+++ week_2/trainee_demo/models/st_detail.py
@@ -1,15 +1,17 @@
+from typing import DefaultDict
 from odoo import  fields, models, api
 
 class Student(models.Model):
     _name = 'student'
     _description = "Student Details"
 
-    name = fields.Char(string="name")
-    # image = fields.Binary(string='image')
-    gender = fields.Selection([ ('male', 'Male'),('female', 'Female'),],'Gender', default='male')
-    branch = fields.Char(string="branch")
-    birthday = fields.Date(string="birthday")
-    email = fields.Char(string="email")
-    enrollment_no = fields.Integer('enrollment_no')
-    contact_no = fields.Integer('contact_no')
-    address = fields.Char(string="address")
+    name                    = fields.Char(string="Name")
+    dob                     = fields.Date(string="DOB")
+    governmentID            = fields.Integer(string="Government ID")
+    contactNo               = fields.Char(string="Contact No")
+    email                   = fields.Char(string= "Email Id")
+    occupation              = fields.Char(string="Occupation")
+    gender                  = fields.Selection([('male','Male'),('female','Female'),],'Gender',default='male')
+    image                   = fields.Binary(string="Image")
+    address                 = fields.Char(string="Address")
+
