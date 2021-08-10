6df06dffcc6d616f70cb82c5c5919f780dcfecb1
Shivam Vaghela <shva@odoo.com>
2021-08-10 10:57:30 +0530

    student

--- student/models/student.py
+++ student/models/student.py
@@ -1,10 +1,19 @@
 from odoo import fields,models
 
 class  student(models.Model):
-	_name = "student.model"
-    _description = "First Model"
+	_name = "student"
+	_description = "student model"
 
-
-    Name = fields.Char(string="Name")
-    Id = fields.Integer(string="id")
-    percentage = fields.float(string="percentage")
+	name = fields.Char(string="name")
+	percentage = fields.Float(string="percentage")
+	place = fields.Char(string="place")
+	image = fields.Binary(string="Image", attachment = True)
+	gender = fields.Selection([('male','male'),
+							   ('female','female')],string="gender")
+	birth_date = fields.Date(string="birth_date")
+	maths = fields.Integer(string="maths")
+	science = fields.Integer(string="science")
+	english = fields.Integer(string="english")
+	total = fields.Float(string="total")
+	start_date = fields.Date(string="start_date")
+	end_date = fields.Date(string="end_date")
