330760a8d65df1a6aa9ba61494f4350275835ce2
Jinal Dod <jido@odoo.com>
2021-08-20 18:37:06 +0530

    Python program practice

--- python_prac/application/app.py
+++ python_prac/application/app.py
@@ -1,13 +1,91 @@
-import json
-f=open("items.json",)
-data=json.load(f)
+import os
+import json  # JavaScript Object Notation, data-interchange lan
+from werkzeug.wrappers import Request, Response
+from werkzeug.middleware.shared_data import SharedDataMiddleware  # serving static files to web server
+from tempfile import gettempdir
+from werkzeug.contrib.sessions import FilesystemSessionStore
+from werkzeug.routing import Map, Rule
+
+def home_page(request):
+    html = open("index.html",'r').read()
+    data = open("items.json",'r').read()
+
+    cartData='false'
+    if bool(int(request.session.__len__())):
+        cartData = request.session['product_ids']
+
+    response = Response(html % {'data':str(json.loads(data)),'cartData':cartData})
+    response.status = '200 ok'
+    response.headers['content-type'] = 'text/html'
+    return response
+
+def my_cart(request,product_id=None):
+
+    if product_id and not bool(int(request.session.__len__())):
+        request.session['product_ids'] = {product_id: product_id}
+    else:
+        request.session['product_ids'].update({product_id: product_id})
+    return home_page(request)
+
+def remove_from_cart(request,product_id=None):
+    
+    if product_id and bool(int(request.session.__len__())):
+        request.session['product_ids'].pop(product_id)
+    return home_page(request)
+Hint Help Save Run Query
 
 
 
-def index(request):
-	for i in data["items"]:
-		l1.append(i)
+url_map = Map([
+    Rule("/", endpoint="home_page"),
+    Rule("/add/<int:product_id>", endpoint="my_cart"),
+    Rule("/remove/<int:product_id>", endpoint="remove_from_cart")
+])
 
-	print(l1)
+views = {
+    "home_page": home_page,
+    "my_cart": my_cart,
+    "remove_from_cart": remove_from_cart,
+}
 
-index(1)
+session_store = FilesystemSessionStore(path=gettempdir())
+
+
+@Request.application
+def application(request):
+    """
+    WSGI application
+    """
+    ses_id = request.cookies.get("session_id")
+    if ses_id is None:
+        request.session = session_store.new()
+    else:
+        request.session = session_store.get(ses_id)
+
+    url_adapter = url_map.bind_to_environ(request.environ)  # bind the url_map to the current request(request.environ)
+    # which will return a new MapAdapter, This url_map adapter can then be used to match or build domains for
+    # the current request.
+
+    endpoint, values = url_adapter.match()  # match() method can then either return a tuple in the form (endpoint, args)
+    # or raise one of the three exceptions NotFound, MethodNotAllowed, or RequestRedirect
+
+    response = views[endpoint](request, **values)
+    session_store.save(request.session)
+    response.set_cookie("session_id", request.session.sid)
+    return response
+
+
+def create_app():
+    my_app = SharedDataMiddleware(application, {
+        '/static': os.path.join(os.path.dirname(__file__), 'static')
+    })
+    return my_app
+
+
+if __name__ == "__main__":
+    from werkzeug.serving import run_simple
+
+    app = create_app()
+    run_simple('localhost', 5000, application,  # start a WSGI application
+               use_reloader=True,
+               use_debugger=True)
--- python_prac/practice/listl.py
+++ python_prac/practice/listl.py
@@ -1,43 +1,44 @@
 a=[4,6,7,2,6,9,612,98]
 
+if __name__ == '__main__':
 
-a[5]=55
-#list method
-print(a)
-print("count num:",a.count(6))
-print(len(a))
-a.append(33)
-print(a)
-print(a.pop())
-print(a)
-b=a.copy()
-print("B    :",b)
-a.insert(4,"hello")
-print(a)
-a.remove("hello")
-a.reverse()
-print(a)
-a.sort()
-print("sort",a)
-a.sort(reverse=True)
-print("desc         :",a)
-
-
-
-# list sliceing
-
-print(a[:])
-print(a[6])
-print(a[2:5])
-print(a[-5:-2])
-print(a[2:])
-print(a[:2])
-print(a[::-1])
-print(a + ["a","b"])
-b=[3,[7,8,9],66]
-print(b[1][1])
+	a[5]=55
+	#list method
+	print(a)
+	print("count num:",a.count(6))
+	print(len(a))
+	a.append(33)
+	print(a)
+	print(a.pop())
+	print(a)
+	b=a.copy()
+	print("B    :",b)
+	a.insert(4,"hello")
+	print(a)
+	a.remove("hello")
+	a.reverse()
+	print(a)
+	a.sort()
+	print("sort",a)
+	a.sort(reverse=True)
+	print("desc         :",a)
 
 
-#list comprehensions
-squares = [x**2 for x in range(11)]
-print(squares)
+
+	# list sliceing
+
+	print(a[:])
+	print(a[6])
+	print(a[2:5])
+	print(a[-5:-2])
+	print(a[2:])
+	print(a[:2])
+	print(a[::-1])
+	print(a + ["a","b"])
+	b=[3,[7,8,9],66]
+	print(b[1][1])
+
+
+	#list comprehensions
+	squares = [x**2 for x in range(11)]
+	print(squares)
