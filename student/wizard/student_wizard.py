295d1a9a5ea2050578127f82661253a18bcac685
Vishva Patel <vipa@odoo.com>
2021-09-15 18:47:09 +0530

    wizard and view inherit

---
+++ student/wizard/student_wizard.py
@@ -0,0 +1,15 @@
+
+from odoo import fields, models, api
+
+
+class Wizards(models.TransientModel):
+    _name = "student.wizard"
+    _description = "Wizard: Quick Registration of College"
+
+
+    college_id = fields.Many2one('student.college', string="College")
+
+
+    def add_college(self):
+        ids = self._context.get("active_ids")
+        return self.env['student'].browse(ids).write({'college_id':self.college_id})
