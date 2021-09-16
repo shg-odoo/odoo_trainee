d139601f12d636473b62cef2d05b8a1944cc1581
Vishva Patel <vipa@odoo.com>
2021-09-16 18:35:27 +0530

    security

--- student/__manifest__.py
+++ student/__manifest__.py
@@ -6,6 +6,8 @@
     'category': 'Management',
     'depends': ['base'],
     'data': [
+            'security/ir.model.access.csv',
+            'security/sequence.xml',
     		'views/st_view.xml',
     		'data/st_data.xml',
             'wizard/student_wizard.xml',
