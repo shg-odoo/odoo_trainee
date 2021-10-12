7e195c970be8c6fcd6548994a460369276e0962d
Adesh Jolhe (adjo) <adjo@odoo.com>
2021-10-12 18:46:06 +0530

    Odoo training day-2

--- Employee/models/employee.py
+++ Employee/models/employee.py
@@ -4,6 +4,12 @@ class Employee(models.Model):
 	_name = "employee"
 	_description = "employee details"
 
-	first_name = fields.Char("FirstName",required = True)
-	middle_name = fields.Char("MiddleName")
-	last_name = fields.Char("LastName",required = True)
+	empcode =  fields.Integer("Employee Id",required = True)
+	first_name = fields.Char("First Name",required = True)
+	middle_name = fields.Char("Middle Name")
+	last_name = fields.Char("Last Name",required = True)
+	Gender = fields.Selection([('male', 'Male'),
+                               ('female', 'Female'),
+                               ('other', 'Other')])
+	dob = fields.Date('Date of Birth')
+	Salary = fields.Float("Salary")
