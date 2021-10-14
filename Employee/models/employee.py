4bb34a98355b01aaaf8943743c31a66487975497
Adesh Jolhe (adjo) <adjo@odoo.com>
2021-10-14 17:47:18 +0530

    Implemented Onchnage and depend

--- Employee/models/employee.py
+++ Employee/models/employee.py
@@ -1,4 +1,5 @@
-from odoo import models,fields
+from odoo import models,fields,api
+from dateutil.relativedelta import relativedelta
 from datetime import datetime,date
 class Employee(models.Model):
 	_name = "employee"
@@ -13,7 +14,23 @@ class Employee(models.Model):
                                ('other', 'Other')])
 	dob = fields.Date('Date of Birth')
 	current_date = fields.Date(string="Current Date",default=lambda s: fields.Date.context_today(s))
-	indian = fields.Boolean('Indian', readonly=True)
+	age = fields.Integer("Age",compute="_get_age",store=True)	
+	indian = fields.Boolean('Indian')
 	contact = fields.Char("Contact Number")
+	hire_date = fields.Date("Date of Joining")
+	branch = fields.Char("Department")
 	Salary = fields.Float("Salary")
 	experiance = fields.Float("Experiance")
+	
+	@api.depends('dob')
+	def _get_age(self):
+		for i in self:
+			if i.dob:
+				i.age = relativedelta(date.today(),i.dob).years
+
+
+
+	@api.onchange('hire_date','current_date')
+	def _get_experiance(self):
+		for i in self:
+			i.experiance = relativedelta(i.current_date,i.hire_date).years
