4d71ebbc6bba7e739771e7506e9d23043adde5f7
Umesh Gupta <umgu@odoo.com>
2021-11-15 18:39:30 +0530

    Views

--- odoo_training_umgu/custom/Student_module/models/student.py
+++ odoo_training_umgu/custom/Student_module/models/student.py
@@ -4,9 +4,6 @@ class Student(models.Model):
 	_name="student.details"
 	_description="Student details"
 
-	# first_name=fields.Char(string="First Name");
-	# last_name=fields.Char(string="Last Name")
-
 	name=fields.Char(string="Name")
 	enrollment_no=fields.Integer(string="Enrollment No")
 	contact_no=fields.Char(string="Contact No")
@@ -14,5 +11,29 @@ class Student(models.Model):
 	branch=fields.Char(string="Branch")
 	dob=fields.Date(string="DOB")
 	gender=fields.Selection([('male','Male'),('female','Female')],'Gender',default='male')
+	age=fields.Integer(String="Age",compute="calculate_age")
+	age_group=fields.Selection([('minor','Minor'),('major','Major')],string="Age Group",compute="calculate_age_group")
 	percentage=fields.Float(string="Percentage")
+	image=fields.Binary(string="Photo")
+	address=fields.Text(string="Address");
+	brief_intro=fields.Html(string="Brief Introduction");
 
+
+
+	@api.depends('dob')
+	def calculate_age(self):
+		for rec in self:
+			if rec.dob:
+				curr_date=date.today()
+				curr_year=curr_date.year
+				rec.age=curr_year-rec.dob.year
+
+
+	@api.depends('age')
+	def calculate_age_group(self):
+		for rec in self:
+			if rec.age:
+				if rec.age>=18:
+					rec.age_group='major'
+				else:
+					rec.age_group='minor'
